8622e55d0edbc7b189c8bf03f7c321d382a1c404
Vitória Raupp <vitoriaraupp0@gmail.com>
2022-10-05 13:41:23 -0300

    novo arquivo

---
+++ pessoa.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// modelagem da estrutura
+typedef struct{
+    char nome [50];
+    int idade;
+    char sexo;
+} Pessoa;
+
+int main(){
+    Pessoa cliente;
+
+    printf("Nome: \n");
+    fgets(cliente.nome, 40, stdin);
+    printf("Sexo: \n");
+    scanf("%c", &cliente.sexo);
+    printf("Idade: \n");
+    scanf("%d", &cliente.idade);
+
+    //cliente.idade = 19;
+    //cliente.sexo = 'F';
+    //strcpy(cliente.nome, "Vitoria");
+
+    printf("Nome: %s Idade %d \nSexo: %c ", cliente.nome, cliente.idade, cliente.sexo);
+
+return 0;
+}
